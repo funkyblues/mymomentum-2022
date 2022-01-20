30ea4d3fa184229eb5e0427a9674456d79599e17
funkyblues <dnr8874@naver.com>
2022-01-21 02:31:22 +0900

    To-Do 정리 후 다시 시작

--- js/time.js
+++ js/time.js
@@ -1,10 +1,18 @@
+import { user } from user;
+
 const clock = document.querySelector(".time");
+const greetings = document.querySelector(".greeting .center-col");
 
 function setClock() {
 	const date = new Date();
 	const hours = String(date.getHours()).padStart(2, "0");
 	const minutes = String(date.getMinutes()).padStart(2, "0");
 	clock.innerText = `${hours}:${minutes}`;
+	
+	if (hours < 12 && hours > 6) {
+		greetings.innerText = `Good Morning ${user}`;
+	}
+
 }
 
 setClock();
